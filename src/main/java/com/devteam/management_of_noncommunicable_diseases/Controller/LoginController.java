f43f33a1e09be7f50a4844d90f85103a733b8c64
thanhLam <nguyenthanhlam02122003@gmail.com>
2023-09-20 19:39:54 +0700

    Loggin 1.0

--- src/main/java/com/devteam/management_of_noncommunicable_diseases/Controller/LoginController.java
+++ src/main/java/com/devteam/management_of_noncommunicable_diseases/Controller/LoginController.java
@@ -1,4 +1,22 @@
 package com.devteam.management_of_noncommunicable_diseases.Controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
 public class LoginController {
+    @FXML
+    private ImageView btnCloseLogin;
+
+    @FXML
+    private Button btnLogin;
+
+    @FXML
+    private PasswordField passField;
+
+    @FXML
+    private TextField userField;
+
 }
--- src/main/java/com/devteam/management_of_noncommunicable_diseases/HelloApplication.java
+++ src/main/java/com/devteam/management_of_noncommunicable_diseases/HelloApplication.java
@@ -4,14 +4,16 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("View/hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("View/Login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.initStyle(StageStyle.TRANSPARENT);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
