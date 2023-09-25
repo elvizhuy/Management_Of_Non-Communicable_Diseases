70212c31754e9ba1289e5f00cd561ed7a972c07d
HuyNg <elvizhuy@gmail.com>
2023-09-25 21:19:56 +0700

    update function

---
+++ src/main/java/com/devteam/management_of_noncommunicable_diseases/Model/Disease.java
@@ -0,0 +1,4 @@
+package com.devteam.management_of_noncommunicable_diseases.Model;
+
+public class Disease {
+}
---
+++ src/main/java/com/devteam/management_of_noncommunicable_diseases/Model/People.java
@@ -0,0 +1,5 @@
+package com.devteam.management_of_noncommunicable_diseases.Model;
+
+public class People {
+
+}
