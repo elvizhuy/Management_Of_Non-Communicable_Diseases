46cbbd53b699062394a05de6e7dc6bc39a60f0c5
elvizhuy <elvizhuy@gmail.com>
2023-09-26 13:52:01 +0700

    add class people

--- src/main/java/com/devteam/management_of_noncommunicable_diseases/Model/People.java
+++ src/main/java/com/devteam/management_of_noncommunicable_diseases/Model/People.java
@@ -1,5 +1,93 @@
 package com.devteam.management_of_noncommunicable_diseases.Model;
 
-public class People {
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
+public class People {
+    String name;
+    LocalDate dateOfBirth;
+    Enum gender;
+    String address;
+    String phoneNumber;
+    String email;
+    String note;
+    LocalDateTime deleted_at;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Enum getGender() {
+        return gender;
+    }
+
+    public void setGender(Enum gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public LocalDateTime getDeleted_at() {
+        return deleted_at;
+    }
+
+    public void setDeleted_at(LocalDateTime deleted_at) {
+        this.deleted_at = deleted_at;
+    }
+
+    public People () {
+
+    }
+    public People(String name, LocalDate dateOfBirth, Enum gender, String address, String phoneNumber, String email, String note, LocalDateTime deleted_at) {
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.note = note;
+        this.deleted_at = deleted_at;
+    }
 }
