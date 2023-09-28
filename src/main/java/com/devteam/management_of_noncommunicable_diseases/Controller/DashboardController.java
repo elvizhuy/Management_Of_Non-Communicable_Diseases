6af88ed7ddb667151ea9cdf1128c97a2119d09eb
HuyNg <elvizhuy@gmail.com>
2023-09-28 19:52:20 +0700

    .

--- src/main/java/com/devteam/management_of_noncommunicable_diseases/Controller/DashboardController.java
+++ src/main/java/com/devteam/management_of_noncommunicable_diseases/Controller/DashboardController.java
@@ -1,24 +1,23 @@
 package com.devteam.management_of_noncommunicable_diseases.Controller;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.Objects;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class DashboardController implements Initializable {
+public class DashboardController extends Thread implements Initializable {
 
     @FXML
     private BorderPane bp;
@@ -50,7 +49,9 @@ public class DashboardController implements Initializable {
 
     @FXML
     void addStaff(ActionEvent event) throws IOException {
-        loadView("/com/devteam/management_of_noncommunicable_diseases/View/addStaffs.fxml");
+        new Thread(() -> {
+            Platform.runLater(this::loadView);
+        }).start();
     }
 
     @FXML
@@ -65,18 +66,22 @@ public class DashboardController implements Initializable {
         });
 
     }
-    private void loadView(String fxmlFileName) {
-        try {
-            // Load FXML file
-            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlFileName));
-            Parent root = loader.load();
+    private void loadView() {
+        new Thread(()->{
+            Platform.runLater(()->{
+                try {
+                    // Load FXML file
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/addStaffs.fxml"));
+                    Parent root = loader.load();
 
-            // Set the loaded view to the ContentArea
-            ContentArea.getChildren().clear();
-            ContentArea.getChildren().add(root);
-        } catch (IOException e) {
-            Logger.getLogger(DashboardController.class.getName()).log(Level.SEVERE, null, e);
-        }
+                    // Set the loaded view to the ContentArea
+                    ContentArea.getChildren().clear();
+                    ContentArea.getChildren().add(root);
+                } catch (IOException e) {
+                    Logger.getLogger(DashboardController.class.getName()).log(Level.SEVERE, null, e);
+                }
+            });
+        }).start();
     }
 
 }
